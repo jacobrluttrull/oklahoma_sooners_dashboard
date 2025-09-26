cecaaaff31993d4fcf7a9a37abb3321aae96b8d6
Jacob Luttrull <jacobrluttrull@gmail.com>
2025-09-26 02:17:18 -0400

    made hello world in Django for the first time will work on this more over the weekend and all of next week.

--- oklahoma_dashboard/asgi.py
+++ oklahoma_dashboard/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for OklahomaSooners project.
+ASGI config for oklahoma_dashboard project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OklahomaSooners.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oklahoma_dashboard.settings')
 
 application = get_asgi_application()
